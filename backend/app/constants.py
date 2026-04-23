378d52e841e188f890a301d169bf995abd026f16
Mokitha-Sakthi <mokithasakthi@gmail.com>
2026-04-23 19:51:48 +0530

    feat: enhance suspects with colors and metadata

---
+++ backend/app/constants.py
@@ -0,0 +1,31 @@
+SUSPECTS = {
+    "Miss Scarlett": {"color": "#E53935", "description": "The femme fatale"},
+    "Colonel Mustard": {"color": "#FDD835", "description": "The retired military officer"},
+    "Mrs. White": {"color": "#FAFAFA", "description": "The domestic housekeeper"},
+    "Mr. Green": {"color": "#43A047", "description": "The businessman"},
+    "Mrs. Peacock": {"color": "#1E88E5", "description": "The socialite"},
+    "Professor Plum": {"color": "#8E24AA", "description": "The intellectual"}
+}
+
+WEAPONS = [
+    "Candlestick",
+    "Dagger",
+    "Lead Pipe",
+    "Revolver",
+    "Rope",
+    "Wrench"
+]
+
+ROOMS = [
+    "Kitchen",
+    "Ballroom",
+    "Conservatory",
+    "Dining Room",
+    "Billiard Room",
+    "Library",
+    "Lounge",
+    "Hall",
+    "Study"
+]
+
+SUSPECT_NAMES = list(SUSPECTS.keys())
